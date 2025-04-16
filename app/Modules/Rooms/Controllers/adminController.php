da02541a4b1099edff697bfdff6cd0e35ff1e6ef
Ali Elbialy <110687260+alielbialy4@users.noreply.github.com>
2025-04-16 03:40:10 +0200

    push

--- app/Modules/Rooms/Controllers/adminController.php
+++ app/Modules/Rooms/Controllers/adminController.php
@@ -2,18 +2,28 @@
 
 namespace App\Modules\Rooms\Controllers;
 
+use App\Modules\Rooms\Models\Rooms;
+use App\Http\Controllers\Controller;
 use App\Modules\Rooms\Requests\StoreRequest;
 use App\Modules\Rooms\Services\RoomsService;
 use App\Http\Controllers\AdminBaseController;
 use App\Modules\Rooms\Requests\UpdateRequest;
+use App\Modules\Rooms\Resources\RoomsResource;
 
-class AdminController extends AdminBaseController
+class AdminController extends Controller
 {
-    public function __construct()
+    // public function __construct()
+    // {
+    //     $this->service       = new RoomsService();
+    //     $this->StoreRequest  = new StoreRequest();
+    //     $this->UpdateRequest = new UpdateRequest();
+    // }
+
+    // index
+    public function index()
     {
-        $this->service       = new RoomsService();
-        $this->StoreRequest  = new StoreRequest();
-        $this->UpdateRequest = new UpdateRequest();
+        $rooms = Rooms::all();
+        return $this->sendResponse(RoomsResource::collection($rooms), 'Rooms retrieved successfully');
     }
 
 }
