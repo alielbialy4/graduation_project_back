9bf2a52790a5dfc879f3ffa66f39fd3ec6953f8d
Ali Elbialy <110687260+alielbialy4@users.noreply.github.com>
2025-04-23 01:35:47 +0200

    push

--- app/Modules/Mcp_Users/routes/routes.php
+++ app/Modules/Mcp_Users/routes/routes.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Modules\Mcp_Users\Controllers\adminController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('mcp/users', [adminController::class, 'index']);
-    Route::get('mcp/users/{id}', [adminController::class, 'show']);
+    Route::get('/mcp/users', [adminController::class, 'index']);
+    Route::get('/mcp/users/{id}', [adminController::class, 'show']);
     // delete user
-    Route::delete('mcp/users/{id}', [adminController::class, 'delete']);
+    Route::delete('/mcp/users/{id}', [adminController::class, 'delete']);
 
 });
